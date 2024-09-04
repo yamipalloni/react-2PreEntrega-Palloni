a467f1961165e03ca1ea2760f89ce1bdca9fbccd
Yamila Palloni <ypalloni@gmail.com>
2024-09-04 09:50:21 -0300

    version1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+
+const App = () => {
+  return (
+    <ChakraProvider>
+      <NavBar />
+      <ItemListContainer greeting="Bienvenidos a mi tienda"/>
+    </ChakraProvider>
+  );
+};
+
+export default App;
